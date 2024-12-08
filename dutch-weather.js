d7c4a6671e56102c5642d0abbeedfc7f84713cf5
Gilles van den Hoven <gilles@webunity.nl>
2024-12-08 20:59:53 +0100

    Updated formatting.

--- dutch-weather.js
+++ dutch-weather.js
@@ -1,90 +1,85 @@
 function checkInterval(iv, df) {
-	if (Math.sign(iv) === NaN) {
-		return df;
-	} else if (Math.sign(iv) === -1) {
-		return -1;
-	}
-	return parseInt(iv);
+  if (Math.sign(iv) === NaN) {
+    return df
+  } else if (Math.sign(iv) === -1) {
+    return -1
+  }
+  return parseInt(iv)
 }
 
-module.exports = function(RED) {
-	"use strict";
+module.exports = function (RED) {
+  "use strict"
 
-	const WeatherLogic = require('./lib/WeatherLogic');
+  const WeatherLogic = require("./lib/WeatherLogic")
 
-	/**
-	 * Configuration node which holds only latitude and longitude.
-	 */
-	function dutchWeatherConf(n) {
-		RED.nodes.createNode(this, n);
+  /**
+   * Configuration node which holds only latitude and longitude.
+   */
+  function dutchWeatherConf(n) {
+    RED.nodes.createNode(this, n)
 
-		// Properties
-		this.lat = n.lat;
-		this.lng = n.lng;
+    // Properties
+    this.lat = n.lat
+    this.lng = n.lng
 
-		// Methods
-		var node = this;
+    // Methods
+    var node = this
 
-		// Create object
-		this.weatherLogic = null;
-		this.weatherLogic = new WeatherLogic(this.lat, this.lng);
-	}
-	RED.nodes.registerType("dutch-weather-conf", dutchWeatherConf);
+    // Create object
+    this.weatherLogic = null
+    this.weatherLogic = new WeatherLogic(this.lat, this.lng)
+  }
+  RED.nodes.registerType("dutch-weather-conf", dutchWeatherConf)
 
+  /**
+   * Rain events node
+   */
+  function dutchWeatherRainState(n) {
+    RED.nodes.createNode(this, n)
+    this.conf = RED.nodes.getNode(n.conf)
 
+    if (!this.conf) {
+      return null
+    }
 
-	/**
-	 * Rain events node
-	 */
-	function dutchWeatherRainState(n) {
-		RED.nodes.createNode(this, n);
-		this.conf = RED.nodes.getNode(n.conf);
+    var node = this
+    this.conf.weatherLogic.on("rain-error", function (msg) {
+      node.debug(msg)
+    })
 
-		if (!this.conf) {
-			return null;
-		}
+    this.conf.weatherLogic.on("rain-state", function (rainState) {
+      node.send({ topic: "rain-state", payload: rainState })
+    })
 
-		var node = this;
-		this.conf.weatherLogic.on('rain-error', function (msg) {
-			node.debug(msg);
-		});
+    node.on("input", function (msg) {
+      if (msg && msg.hasOwnProperty("payload") && msg.payload.trigger == true) {
+        node.conf.weatherLogic.checkRain()
+      }
+    })
+  }
+  RED.nodes.registerType("dutch-weather-rain-state", dutchWeatherRainState)
 
-		this.conf.weatherLogic.on('rain-state', function (rainState) {
-			node.send({ 'topic': 'rain-state', 'payload': rainState});
-		});
+  /**
+   * Meteo events node
+   */
+  function dutchWeatherMeteoplaza(n) {
+    RED.nodes.createNode(this, n)
+    this.conf = RED.nodes.getNode(n.conf)
 
-		node.on('input', function(msg) {
-			if (msg && msg.hasOwnProperty('payload') && (msg.payload.trigger == true)) {
-				node.conf.weatherLogic.checkRain();
-			}
-		});
-	}
-	RED.nodes.registerType("dutch-weather-rain-state", dutchWeatherRainState);
+    if (!this.conf) {
+      return null
+    }
 
+    var node = this
+    this.conf.weatherLogic.on("meteoplaza", function (meteoplaza) {
+      node.send({ topic: "meteoplaza", payload: meteoplaza })
+    })
 
-
-	/**
-	 * Meteo events node
-	 */
-	function dutchWeatherMeteoplaza(n) {
-		RED.nodes.createNode(this, n);
-		this.conf = RED.nodes.getNode(n.conf);
-
-		if (!this.conf) {
-			return null;
-		}
-
-		var node = this;
-		this.conf.weatherLogic.on('meteoplaza', function (meteoplaza) {
-			node.send({ 'topic': 'meteoplaza', 'payload': meteoplaza });
-		});
-
-		node.on('input', function(msg) {
-			if (msg && msg.hasOwnProperty('payload') && (msg.payload.trigger == true)) {
-				node.conf.weatherLogic.updateMeteoplaza(true);
-			}
-		});
-
-	}
-	RED.nodes.registerType("dutch-weather-meteoplaza", dutchWeatherMeteoplaza);
+    node.on("input", function (msg) {
+      if (msg && msg.hasOwnProperty("payload") && msg.payload.trigger == true) {
+        node.conf.weatherLogic.updateMeteoplaza(true)
+      }
+    })
+  }
+  RED.nodes.registerType("dutch-weather-meteoplaza", dutchWeatherMeteoplaza)
 }
--- local-test.js
+++ local-test.js
@@ -1,69 +1,81 @@
-'use strict';
+"use strict"
 
-const Util = require('util');
+const Util = require("util")
 
 // Variable to be used in the exitHandler
-var Weather;
+var Weather
 
 /**
  * exit handler function
  */
 function exitHandler(options, err) {
-	if (options.cleanup) {
-		// Perform cleanup tasks
-	}
+  if (options.cleanup) {
+    // Perform cleanup tasks
+  }
 
-	if (err) {
-		console.log(err.stack);
-	}
+  if (err) {
+    console.log(err.stack)
+  }
 
-	if (options.exit) {
-		process.exit();
-	}
+  if (options.exit) {
+    process.exit()
+  }
 }
 
 // process handlers
-process.on('exit', exitHandler.bind(null, { cleanup: true }));
-process.on('SIGINT', exitHandler.bind(null, { exit: true }));
-process.on('SIGUSR1', exitHandler.bind(null, { exit: true })); // catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR2', exitHandler.bind(null, { exit: true })); // catches "kill pid" (for example: nodemon restart)
-process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
-process.on('unhandledRejection', (reason, p) => {
-	console.log('Unhandled Rejection at: Promise', p);
-	console.log('Reason:', reason);
-});
+process.on("exit", exitHandler.bind(null, { cleanup: true }))
+process.on("SIGINT", exitHandler.bind(null, { exit: true }))
+process.on("SIGUSR1", exitHandler.bind(null, { exit: true })) // catches "kill pid" (for example: nodemon restart)
+process.on("SIGUSR2", exitHandler.bind(null, { exit: true })) // catches "kill pid" (for example: nodemon restart)
+process.on("uncaughtException", exitHandler.bind(null, { exit: true }))
+process.on("unhandledRejection", (reason, p) => {
+  console.log("Unhandled Rejection at: Promise", p)
+  console.log("Reason:", reason)
+})
 
 // Start script
-console.log('---------------------------------------------');
-console.log(' node-red-contrib-dutch-weather local test');
-console.log('---------------------------------------------');
+console.log("---------------------------------------------")
+console.log(" node-red-contrib-dutch-weather local test")
+console.log("---------------------------------------------")
 
 // Check Node version
-console.log('* verifying installed Node runtime ...');
+console.log("* verifying installed Node runtime ...")
 if (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 6.0) {
-	throw new Error('This software only runs on node >= 6.0. Your current node version is ' + process.versions.node + '.');
+  throw new Error(
+    "This software only runs on node >= 6.0. Your current node version is " +
+      process.versions.node +
+      "."
+  )
 } else {
-	console.log('  - OK: ', process.versions.node);
+  console.log("  - OK: ", process.versions.node)
 }
 
 // Load modules
-console.log('* loading modules ...');
-const WeatherLogic = require('./lib/WeatherLogic');
-console.log('  - OK');
+console.log("* loading modules ...")
+const WeatherLogic = require("./lib/WeatherLogic")
+console.log("  - OK")
 
 // Start script
-console.log('* Watchting the weather ...');
-var Weather = new WeatherLogic(51.42408, 5.442794);
+console.log("* Watchting the weather ...")
+var Weather = new WeatherLogic(51.42408, 5.442794)
 
-Weather.on('rain-state', function (prediction) {
-	console.log('* Rain state update:');
-	console.log('  - ' + JSON.stringify(prediction, null, '\t'));
-});
+Weather.on("rain-state", function (prediction) {
+  console.log("* Rain state update:")
+  console.log("  - " + JSON.stringify(prediction, null, "\t"))
+  setTimeout(function () {
+    Weather.checkRain()
+  }, 30000)
+})
 
-Weather.on('meteoplaza', function (meteoplaza) {
-	console.log('* Meteoplaza update:');
-	console.log('  - ' + JSON.stringify(meteoplaza, null, '\t'));
-});
+Weather.on("meteoplaza", function (meteoplaza) {
+  console.log("* Meteoplaza update:")
+  console.log("  - " + JSON.stringify(meteoplaza, null, "\t"))
+  setTimeout(function () {
+    Weather.updateMeteoplaza()
+  }, 30000)
+})
 
-setTimeout(function() { Weather.checkRain(); }, 1000);
-setTimeout(function() { Weather.updateMeteoplaza(); }, 1000);
+Weather.checkRain()
+Weather.updateMeteoplaza()
+
+setInterval(() => {}, 1 << 30)
